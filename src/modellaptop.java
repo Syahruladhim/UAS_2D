9dbf09cca0155bfa4fd2b80d63cd524b1afd8d55
Syahruladhim <117799089+Syahruladhim@users.noreply.github.com>
2023-07-14 19:06:29 +0700

    Update modellaptop.java

--- src/modellaptop.java
+++ src/modellaptop.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class modellaptop {
+public class modellaptop {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return Rating;
+    }
+
+    public void setRating(String rating) {
+        Rating = rating;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    private String id;
+    private String Rating;
+    private String nama;
+    private String harga;
 }
+
